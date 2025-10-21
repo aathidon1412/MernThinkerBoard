a5eec13ebc03c0b17523fde662b2f47670925ee7
aathidon1412 <aathikesavaasundararajan@gmail.com>
2025-10-21 22:47:17 +0530

    MernThinker Mudichitomm

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,24 +4,36 @@ import connectDB from "./config/db.js"
 import dotenv from "dotenv"
 import cors from "cors" //Cross Origin Resource Sharing
 import rateLimiter from "./middleware/rateLimiter.js";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-connectDB();
+const __dirname = path.resolve();
 
-app.use(cors(
-    {
-        origin : "http://localhost:5173",
-    }
-));
+if(process.env.NODE_ENV !== "production"){
+    app.use(cors(
+        {
+            origin : "http://localhost:5173",
+        }
+    ));
+}
 app.use(express.json()); //Middleware
 app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*", (req,res) => {
+        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
+    });
+}
 
-//Listening to the server
-app.listen(PORT, ()=>{
-    console.log("Server started!! at PORT:", PORT);
-})
+//Connecting to DataBase
+connectDB().then(()=>{
+    //Listening to the server
+    app.listen(PORT, ()=>{
+        console.log("Server started!! at PORT:", PORT);
+    });
+});
